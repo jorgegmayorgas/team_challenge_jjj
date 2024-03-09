aea610be0baddc7dc1d38b8de43ce880c046d51a
Jorge Giménez <jorgegmayorgas@gmail.com>
2024-03-09 12:38:27 +0100

    20240309_1238

--- toolbox_ML_jorge.py
+++ toolbox_ML_jorge.py
@@ -7,48 +7,195 @@ import seaborn as sns
 from scipy.stats import ttest_ind,mannwhitneyu,ttest_rel,ttest_1samp
 from scipy.stats import chi2_contingency,f_oneway
 
-def get_features_num_classification (dataframe:pd.DataFrame,target_col:str,pvalue:float=0.5):
+def internal_anova_columns (df:pd.DataFrame,target_col:str,lista_num_columnas:list=[],pvalue:float=0.05):
     """
-    Función que devuelve un Dataframe  de Pandas con:
-
-        * El tipo de datos por columna
-    
-        * El porcentaje de nulos
-        
-        * La cantidad de valores únicos
-        
-        * La cardinalidad de la columna en porcentaje
+    Uso interno, devuelve una lista con las columnas del dataframe cuyo ANOVA con la columna designada por "target_col" 
+    supere el test de hipótesis con significación mayor o igual a 1-pvalue
 
     Argumentos:
 
-    `df` (DataFrame): Variable dataframe de Pandas a describir.
-    
+    `df` (DataFrame): Variable dataframe de Pandas.
+    `target_col` (str): Variable target tipo str.
+    `pvalue` (float): Variable float con valor por defecto 0.05
 
     Retorna:
-    pandas.DataFrame: Dataframe
+    col_nums: List
     """
+    selected_columns=[]
+    for columna in lista_num_columnas:
+        unique_values=df[columna].unique()
+        for valor in unique_values:
+            t_statistic,p_value = f_oneway(df[target_col][df[columna]==valor],df[target_col])
+            if p_value<pvalue:#corregido
+                if columna not in selected_columns:
+                    selected_columns.append(columna)
+    return selected_columns
+
+def get_features_num_classification (df:pd.DataFrame,target_col:str,pvalue:float=0.05):
+    """
+    La función devuelve una lista con las columnas numéricas del dataframe cuyo ANOVA con la columna designada por "target_col" 
+    supere el test de hipótesis con significación mayor o igual a 1-pvalue
+
+    Argumentos:
+
+    `df` (DataFrame): Variable dataframe de Pandas.
+    `target_col` (str): Variable target tipo str.
+    `pvalue` (float): Variable float con valor por defecto 0.05.
+
+    Retorna:
+    col_nums: List
+    """
+    #Validaciones
+    
+    if target_col=="":
+        raise ValueError("El parámetro target_col no puede estar vacío")
+    if target_col not in df.columns:
+        raise ValueError("El parámetro target_col debe estar en el DataFrame")
+    #Búsqueda de columnas numéricas
+    num_values=['float32','int32','float64','int64','int8', 'int16', 'float16','uint8', 'uint16', 'uint32', 'uint64']
+    lista_num_columnas=[]
+    for columna in df.columns:
+        if columna.dtype in num_values:
+            lista_num_columnas.append(columna)
+    #ANOVA
+    selected_columns=[]
+    selected_columns= internal_anova_columns(df,target_col,lista_num_columnas,pvalue)
+    '''
+    for columna in lista_num_columnas:
+        unique_values=df[columna].unique()
+        for valor in unique_values:
+            t_statistic,p_value = f_oneway(df[target_col][df[columna]==valor],df[target_col])
+            if p_value<pvalue:#corregido
+                if columna not in selected_columns:
+                    selected_columns.append(columna)
+    '''
+    return selected_columns
+
+def plot_features_num_classification (df:pd.DataFrame,target_col:str="",columns:list=[],pvalue:float=0.5):
+    """
+    La función pinta una pairplot del DataFrame considerando la columna designada por "target_col" y aquellas incluidas en "column" que cumplan el test de ANOVA 
+    para el nivel 1-pvalue de significación estadística. La función devolverá los valores de "columns" que cumplan con las condiciones anteriores. 
+    Se espera que las columnas sean numéricas. El pairplot utiliza como argumento de hue el valor de target_col
+    Si target_Col es superior a 5, se usan diferentes pairplot diferentes, se pinta un pairplot por cada 5 valores de target posibles.
+    Si la lista de columnas a pintar es grande se pinten varios pairplot con un máximo de cinco columnas en cada pairplot,
+    siendo siempre una de ellas la indicada por "target_col"
+    Argumentos:
+
+    `df` (DataFrame): Variable dataframe de Pandas.
+    `target_col` (str): Variable target tipo str.
+    `columns` (list): Variable con la lista de columnas de tipo list.
+    `pvalue` (float): Variable float con valor por defecto 0.5.    
+    
+    Retorna:
+    sns.pairplot: Pairplot
+    """
+    selected_columns= internal_anova_columns(df,target_col,columns,pvalue)
+    if len(selected_columns)>0:
+        if len(selected_columns)>5:
+            #codigo para bloques de 5
+            pass
+        else:
+            #unico
+            pass
     return None
 
-def plot_features_num_classification (dataframe:pd.DataFrame,target_col:str="",columns:list=[],pvalue:float=0.5):
-    """
-    Función que devuelve un Dataframe  de Pandas con:
-
-        * El tipo de datos por columna
+#Javier
+def get_features_cat_classification(df:pd.DataFrame, target_col:str, mi_threshold:float=0.0, normalize:bool=False):
+    '''
+    Esta función recibe como argumentos un dataframe, el nombre de una de las columnas del mismo (argumento 'target_col'), que debería ser el target de un hipotético 
+    modelo de clasificación, es decir debe ser una variable categórica o numérica discreta pero con baja cardinalidad, un argumento "normalize" con valor False por defecto, 
+    una variable float "mi_threshold" cuyo valor por defecto será 0.
+    * En caso de que "normalize" sea False:
+        La función debe devolver una lista con las columnas categóricas del dataframe cuyo valor de mutual information con 'target_col' iguale o supere 
+        el valor de "mi_threshold".
+    * En caso de que "relative" sea True:
+        La función debe devolver una lista con las columnas categóricas del dataframe cuyo valor normalizado de mutual information con 'target_col' iguale o supere 
+        el valor de "mi_threshold". 
+        El valor normalizado de mutual information se considera el obtenido de dividir el valor de mutual information tal cual ofrece sklearn o la fórmula de cálculo 
+        por la suma de todos los valores de mutual information de las features categóricas del dataframe.
+    En este caso, la función debe comprobar que "mi_threshold" es un valor float entre 0 y 1, y arrojar un error si no lo es.
+    La función debe hacer todas las comprobaciones necesarias para no dar error como consecuecia de los valores de entrada.
+    Es decir hará un check de los valores asignados a los argumentos de entrada y si estos no son adecuados debe retornar None y printar 
+    por pantalla la razón de este comportamiento. Ojo entre las comprobaciones debe estar que "target_col" hace referencia a una variable numérica continua del dataframe.
     
-        * El porcentaje de nulos
-        
-        * La cantidad de valores únicos
-        
-        * La cardinalidad de la columna en porcentaje
-
     Argumentos:
 
-    `df` (DataFrame): Variable dataframe de Pandas a describir.
+    `df` (DataFrame): Variable dataframe de Pandas.
+    `target_col` (str): Variable target tipo str.
+    `mi_threshold` (float): Variable float con valor por defecto 0.0.
+    `normalize` (bool): Variable bool con valor por defecto False.
     
-
     Retorna:
-    pandas.DataFrame: Dataframe
+    selected_columns: List
+
+    '''
+    # Comprobación de argumentos de entrada
+    if not isinstance(df, pd.DataFrame):
+        print("Error: 'df' debe ser un DataFrame de pandas.")
+        return None
+    
+    if not isinstance(target_col, str):
+        print("Error: 'target_col' debe ser una cadena de caracteres.")
+        return None
+    
+    if target_col not in df.columns:
+        print("Error: 'target_col' no está presente en el DataFrame.")
+        return None
+    
+    if df[target_col].dtype not in ['object', 'category']:
+        print("Error: 'target_col' debe ser una variable categórica o numérica discreta del DataFrame.")
+        return None
+    
+    if not isinstance(mi_threshold, float):
+        print("Error: 'mi_threshold' debe ser un valor float.")
+        return None
+    
+    if not 0 <= mi_threshold <= 1 and normalize:
+        print("Error: 'mi_threshold' debe estar entre 0 y 1 cuando 'normalize' es True.")
+        return None
+    
+def plot_features_cat_classification(df:pd.DataFrame, target_col:str="", columns:list=[], mi_threshold:float=0.0, normalize:bool=False):
     """
+    La función pinta una pairplot del DataFrame considerando la columna designada por "target_col" y aquellas incluidas en "column" que cumplan el test de ANOVA 
+    para el nivel 1-pvalue de significación estadística. La función devolverá los valores de "columns" que cumplan con las condiciones anteriores. 
+    Se espera que las columnas sean numéricas. El pairplot utiliza como argumento de hue el valor de target_col
+    Si target_Col es superior a 5, se usan diferentes pairplot diferentes, se pinta un pairplot por cada 5 valores de target posibles.
+    Si la lista de columnas a pintar es grande se pinten varios pairplot con un máximo de cinco columnas en cada pairplot,
+    siendo siempre una de ellas la indicada por "target_col"
+    Argumentos:
+
+    `df` (DataFrame): Variable dataframe de Pandas.
+    `target_col` (str): Variable target tipo str.
+    `columns` (list): Variable con la lista de columnas de tipo list.
+    `pvalue` (float): Variable float con valor por defecto 0.5.    
+    
+    Retorna:
+    sns.pairplot: Pairplot
+    """
+    if not isinstance(df, pd.DataFrame):
+        print("Error: 'df' debe ser un DataFrame de pandas.")
+        return
+    
+    if not isinstance(target_col, str):
+        print("Error: 'target_col' debe ser una cadena de caracteres.")
+        return
+    
+    if target_col and df[target_col].dtype not in ['object', 'category']:
+        print("Error: 'target_col' debe ser una variable categórica del DataFrame.")
+        return
+    
+    if not isinstance(columns, list):
+        print("Error: 'columns' debe ser una lista de strings.")
+        return
+    
+    if not 0 <= mi_threshold <= 1 and normalize:
+        print("Error: 'mi_threshold' debe estar entre 0 y 1 cuando 'normalize' es True.")
+        return
+    
+    if not columns:
+        categorical_cols = df.select_dtypes(include=['object', 'category']).columns
+    else:
+        categorical_cols = columns
     return None
 
 #version inicial toolbox_ML.py
